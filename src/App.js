6c27a014bb6651dcc0a16a234f111785b7f55bd7
KarlNersisyan <karlennersisyan814@gmail.com>
2021-08-03 15:18:25 +0400

    change

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+import Home from './components/Home/Home';
 
 export default function App() {
   return (
-    <div className="text-center App">
-      <p> Hello</p>
+    <div>
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Login from '../Authentication/Login/Login';
+import Navbar from '../Navbar/Navbar';
+
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+      <Login />
+    </div>
+  );
+}
