0140cdf414c79cfafc0d0563c6b772658ed83fe5
KarlNersisyan <karlennersisyan814@gmail.com>
2021-08-03 17:48:29 +0400

    refactor(NavBar): refactor(Navbar): Navbar styles and structure

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import Login from '../Authentication/Login/Login';
 import Navbar from '../Navbar/Navbar';
 
 export default function App() {
   return (
     <div>
       <Navbar />
-      <Login />
     </div>
   );
 }
