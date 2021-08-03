3ba7f3da4dbc8ec09bb8647e9852426a14e3ce61
KarlNersisyan <karlennersisyan814@gmail.com>
2021-08-03 19:19:55 +0400

    feat(components): Added MovieCard component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
+import MovieCard from '../MovieCard/MovieCard';
 
 export default function App() {
   return (
     <div>
       <Navbar />
+      <MovieCard />
     </div>
   );
 }
