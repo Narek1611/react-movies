4327c9dc8dcbb863a31b2a7cccba6126adc9ed07
KarlNersisyan <karlennersisyan814@gmail.com>
2021-08-04 16:47:23 +0400

    feat(helpers): find genre name

---
+++ src/helpers/findGenreName.js
@@ -0,0 +1,11 @@
+export function findGenreName(genres, genreIds) {
+  const res = [];
+  genreIds.forEach((genreId) => {
+    genres.forEach((genre) => {
+      if (genreId === genre.id) {
+        res.push(genre.name);
+      }
+    });
+  });
+  return res;
+}
