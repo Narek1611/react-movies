0e7fc58610f4b8e7e28fd5653492e29f0446e188
KarlNersisyan <karlennersisyan814@gmail.com>
2021-08-04 16:44:37 +0400

    feat(MovieCard): Change styles

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -4,7 +4,6 @@ import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import FavoriteIcon from '@material-ui/icons/Favorite';
@@ -13,6 +12,9 @@ import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
   root: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
     maxWidth: '63vh',
     margin: '27px',
     cursor: 'pointer',
@@ -28,25 +30,25 @@ const useStyles = makeStyles((theme) => ({
 export default function RecipeReviewCard(props) {
   const classes = useStyles();
   return (
-    <Link to={`/${props.id}`}>
-      <Card className={classes.root}>
-        <CardHeader title={props.title} subheader={props.releaseDate} />
+    <Card className={classes.root}>
+      <CardHeader title={props.title} subheader={props.releaseDate} />
+      <Link to={`/${props.id}`}>
         <CardMedia
           className={classes.media}
           image={getImgUrl(props.path)}
           title={props.title}
         />
-        <CardContent>
-          <Typography variant="body2" color="textSecondary" component="p">
-            {props.aboutFilm}
-          </Typography>
-        </CardContent>
-        <CardActions disableSpacing>
-          <IconButton>
-            <FavoriteIcon />
-          </IconButton>
-        </CardActions>
-      </Card>
-    </Link>
+      </Link>
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {props.aboutFilm}
+        </Typography>
+      </CardContent>
+      <CardContent>
+        <IconButton>
+          <FavoriteIcon />
+        </IconButton>
+      </CardContent>
+    </Card>
   );
 }
