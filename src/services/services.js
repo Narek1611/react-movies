8aec780b9b7c671f6e65a766446a38a4eafda597
KarlNersisyan <karlennersisyan814@gmail.com>
2021-08-05 23:36:22 +0400

    refactor(serfices): codes optimize

--- src/services/services.js
+++ src/services/services.js
@@ -1,23 +1,33 @@
+import axios from 'axios';
+
 const API_KEY = '2509671e8c6bd78d29677bfb02b414f6';
 const API_URL = 'https://api.themoviedb.org/3/';
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 
+export function getAllMovies() {
+  return axios
+    .get(`${API_URL}movie/popular?api_key=${API_KEY}&language=en-US`)
+    .then((res) => res.data);
+}
+
 export function getMoviesByPage(page) {
-  return fetch(
-    `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${page}`
-  ).then((res) => res.json());
+  return axios
+    .get(
+      `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${page}`
+    )
+    .then((res) => res.data);
 }
 
 export function getMovieById(id) {
-  return fetch(`${API_URL}movie/${id}?api_key=${API_KEY}&language=en-US`).then(
-    (res) => res.json()
-  );
+  return axios
+    .get(`${API_URL}movie/${id}?api_key=${API_KEY}&language=en-US`)
+    .then((res) => res.data);
 }
 
 export function getGenres() {
-  return fetch(
-    `${API_URL}genre/movie/list?api_key=${API_KEY}&language=en-US`
-  ).then((res) => res.json());
+  return axios
+    .get(`${API_URL}genre/movie/list?api_key=${API_KEY}&language=en-US`)
+    .then((res) => res.data);
 }
 
 export function getImgUrl(path) {
@@ -25,7 +35,7 @@ export function getImgUrl(path) {
 }
 
 export function getMoviesByQuery(query) {
-  return fetch(`${API_URL}search/movie?api_key=${API_KEY}&query=${query}`).then(
-    (res) => res.json()
-  );
+  return axios
+    .get(`${API_URL}search/movie?api_key=${API_KEY}&query=${query}`)
+    .then((res) => res.data);
 }
