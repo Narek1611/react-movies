b2acf1e06f30064ec213beb67585dfbd9c914764
KarlNersisyan <karlennersisyan814@gmail.com>
2021-08-06 22:51:20 +0400

    feat(App): Split pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Favorite from './pages/Favorite/Favorite';
+import Register from './pages/Register/Register';
+
 export default function App() {
+  const isAuth = localStorage.getItem('isAuth');
+  
   return (
-    <div>
-      <h1> Hello there</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/home" children={<Home isAuth={isAuth} />} />
+        <Route path="/home/favorites" children={<Favorite isAuth={isAuth} />} />
+        <Route exact path="/" children={<Login isAuth={isAuth} />} />
+        <Route path="/register" component={Register} />
+      </Switch>
+    </Router>
   );
 }
