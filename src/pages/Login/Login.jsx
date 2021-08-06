977392de6d3fffb32e77635af07e447626e490b7
KarlNersisyan <karlennersisyan814@gmail.com>
2021-08-06 23:14:45 +0400

    feat(pages): Added Login page

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,140 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import { Link, Redirect } from 'react-router-dom';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+import { getLocalStorage, setLocalStorage } from '../../helpers/localStorage';
+import { storage } from '../../constants/storage';
+import { Routes } from '../../constants/routes';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%',
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+const validationSchema = yup.object({
+  email: yup
+    .string('Enter your email')
+    .email('Enter a valid email')
+    .required('Email is required'),
+  password: yup
+    .string('Enter your password')
+    .min(8, 'Password should be of minimum 8 characters length')
+    .required('Password is required'),
+});
+
+export default function Login() {
+  const classes = useStyles();
+
+  let history = useHistory();
+
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      if (
+        getLocalStorage(storage.users) &&
+        getLocalStorage(storage.users).some(
+          (user) =>
+            user.email === values.email && user.password === values.password
+        )
+      ) {
+        setLocalStorage(storage.isAuth, true);
+        history.push(Routes.home.url);
+      } else {
+        setLocalStorage(storage.isAuth, false);
+        alert('Wrong email or password');
+      }
+    },
+  });
+
+  return getLocalStorage(storage.isAuth) ? (
+    <Redirect to={Routes.home.url} />
+  ) : (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}></Avatar>
+        <Typography component="h1" variant="h5">
+          Register
+        </Typography>
+        <form
+          onSubmit={formik.handleSubmit}
+          className={classes.form}
+          noValidate
+        >
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoFocus
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            error={formik.touched.email && Boolean(formik.errors.email)}
+            helperText={formik.touched.email && formik.errors.email}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            error={formik.touched.password && Boolean(formik.errors.password)}
+            helperText={formik.touched.password && formik.errors.password}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Submit
+          </Button>
+          <Grid container>
+            <Grid item>
+              <Link variant="body2" to="/register">
+                Don't have an account? Register
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Container>
+  );
+}
