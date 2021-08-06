3ad01d0e7cdcfc306fe5cab5271b26b9b7474a8b
KarlNersisyan <karlennersisyan814@gmail.com>
2021-08-07 01:02:14 +0400

    feat(components): Added Header component and scroll top event

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import Fab from '@material-ui/core/Fab';
+import Zoom from '@material-ui/core/Zoom';
+import Navbar from './Navbar';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    position: 'fixed',
+    bottom: theme.spacing(2),
+    right: theme.spacing(2),
+    zIndex: 3,
+  },
+  backTopFab: {
+    width: '80px',
+    height: '80px',
+  },
+}));
+
+function ScrollTop(props) {
+  const { children, window } = props;
+  const classes = useStyles();
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+    disableHysteresis: true,
+    threshold: 100,
+  });
+
+  const handleClick = (event) => {
+    const anchor = (event.target.ownerDocument || document).querySelector(
+      '#back-to-top-anchor'
+    );
+
+    if (anchor) {
+      anchor.scrollIntoView({ behavior: 'smooth', block: 'center' });
+    }
+  };
+
+  return (
+    <Zoom in={trigger}>
+      <div onClick={handleClick} role="presentation" className={classes.root}>
+        {children}
+      </div>
+    </Zoom>
+  );
+}
+
+ScrollTop.propTypes = {
+  children: PropTypes.element.isRequired,
+  window: PropTypes.func,
+};
+
+export default function Header(props) {
+  const classes = useStyles();
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <AppBar>
+        <Navbar
+          favCount={props.favCount}
+          handleSearchInput={props.handleSearchInput}
+        />
+      </AppBar>
+      <Toolbar id="back-to-top-anchor" />
+      <ScrollTop {...props}>
+        <Fab
+          className={classes.backTopFab}
+          color="primary"
+          size="large"
+          aria-label="scroll back to top"
+        >
+          <ArrowUpwardIcon />
+        </Fab>
+      </ScrollTop>
+    </React.Fragment>
+  );
+}
+
+Header.propTypes = {
+  handleSearchInput: PropTypes.func.isRequired,
+};
