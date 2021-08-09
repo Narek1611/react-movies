f7261af733efcc133e3c37f31276842f371280e1
KarlNersisyan <karlennersisyan814@gmail.com>
2021-08-09 16:18:41 +0400

    feat(Loader): Added new style

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,4 +1,10 @@
 import CircularProgress from '@material-ui/core/CircularProgress';
+import './Loader.css';
+
 export default function Loader() {
-  return <CircularProgress />;
+  return (
+    <div className="loading">
+      <CircularProgress />
+    </div>
+  );
 }
